c265abf77f60591ea74afb170bbcd9d046777395
Sudhanshu <Sudhanshu@Suddu.mshome.net>
2022-09-27 21:27:49 +0530

    completed

--- src/main/java/com/pbms/ProjectBackend/ProjectBackendApplication.java
+++ src/main/java/com/pbms/ProjectBackend/ProjectBackendApplication.java
@@ -1,13 +1,26 @@
 package com.pbms.ProjectBackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class ProjectBackendApplication {
+public class ProjectBackendApplication implements CommandLineRunner{
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.passwordEncoder.encode("test"));
+		
+	}
+
+	
 }
